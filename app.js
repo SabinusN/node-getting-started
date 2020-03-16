c207e09b3513f35d65af26a7d880838a7787d327
Sabinus N <sabinus.neculae@sap.com>
2020-03-16 17:26:17 +0200

    added basic hello world app

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-//to do
+//simple node server
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port)
